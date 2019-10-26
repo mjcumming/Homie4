30d8f169310a11282e90065677bb475ca1ba88d7
mjcumming <mike@4831.com>
2019-10-26 10:51:45 -0500

    arg fix

--- homie/node/property/property_switch.py
+++ homie/node/property/property_switch.py
@@ -6,9 +6,5 @@ tags = ['Switch']
 
 class Property_Switch(Property_Enum):
 
-    def __init__(self, node, id='switch', name = 'Switch', settable = True, retained = True, qos=1, unit = None, data_type= 'enum', data_format = 'ON,OFF', value = None, set_value=None, tags=tags, meta=None):
-        
-        super().__init__(node,id,name,settable,retained,qos,unit,data_type,data_format,value,set_value,tags,meta)
-
- 
-
+    def __init__(self, node, id='switch', name = 'Switch', settable = True, retained = True, qos=1, unit = None, data_type= 'enum', data_format = 'ON,OFF', value = None, set_value=None):
+        super().__init__(node, id,name,settable,retained,qos,unit,data_type,data_format,value,set_value)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='Homie4',
-    version='0.0.4',
+    version='0.0.5',
     description='Homie 4.0.0 Implementation',
     author='Michael Cumming',
     author_email='mike@4831.com',
