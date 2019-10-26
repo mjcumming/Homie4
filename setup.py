3c64bcc85d2bfa49193d8c065fbba529284a96c8
mjcumming <mike@4831.com>
2019-10-26 08:13:07 -0500

    ver

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='Homie4',
-    version='0.0.3',
+    version='0.0.4',
     description='Homie 4.0.0 Implementation',
     author='Michael Cumming',
     author_email='mike@4831.com',
