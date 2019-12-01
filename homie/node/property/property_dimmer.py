3cd0f589c450883d69cb880347477ffa59f67f1d
mjcumming <mike@4831.com>
2019-12-01 11:04:52 -0600

    change dimmer property

--- homie/node/property/property_dimmer.py
+++ homie/node/property/property_dimmer.py
@@ -4,7 +4,7 @@ tags = ['Lighting','Dimmable']
 
 class Property_Dimmer(Property_Float):
 
-    def __init__(self,node, id='dimmer', name = 'Dimmer', settable = True, retained = True, qos=1, unit = '%', data_type= None, data_format = '0:100', value = None, set_value=None, tags=tags, meta={}):
+    def __init__(self,node, id='dimmer', name = 'Dimmer', settable = True, retained = True, qos=1, unit = None, data_type= None, data_format = '0:100', value = None, set_value=None, tags=tags, meta={}):
         
         super().__init__(node,id,name,settable,retained,qos,unit,data_type,data_format,value,set_value,tags,meta)
 
