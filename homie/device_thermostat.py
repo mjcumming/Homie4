f619375645e345edfc2078f971bd886611070126
mjcumming <mike@4831.com>
2019-12-27 11:09:48 -0600

    therm

--- homie/device_thermostat.py
+++ homie/device_thermostat.py
@@ -23,7 +23,7 @@ THERMOSTAT_SETTINGS = {
     'heat_setpoint' : 70,
     'max_heat_setpoint' : 90,
     'min_heat_setpoint' : 60,    
-    'fan_mode' : 'ON',
+    'fan_mode' : 'On',
     'fan_modes' : ['On','Auto'],
     'hold_mode' : 'Schedule',
     'hold_modes' : ['Schedule','Temporary','Permanent'],
@@ -43,22 +43,27 @@ class Device_Thermostat(Device_Base):
         node = (Node_Base(self,'controls','Controls','controls'))
         self.add_node (node)
 
-        heat_setpt_limits = '{}:{}'.format(thermostat_settings['min_heat_setpoint'],thermostat_settings['max_heat_setpoint'])
-        self.heat_setpoint = Property_Setpoint (node,id='heatsetpoint',name='Heat Setpoint',data_format=heat_setpt_limits,unit=thermostat_settings['units'],value=thermostat_settings['heat_setpoint'],set_value = self.set_heat_setpoint)
-        node.add_property (self.heat_setpoint)
+        if 'heat_setpoint' in thermostat_settings:
+            heat_setpt_limits = '{}:{}'.format(thermostat_settings['min_heat_setpoint'],thermostat_settings['max_heat_setpoint'])
+            self.heat_setpoint = Property_Setpoint (node,id='heatsetpoint',name='Heat Setpoint',data_format=heat_setpt_limits,unit=thermostat_settings['units'],value=thermostat_settings['heat_setpoint'],set_value = self.set_heat_setpoint)
+            node.add_property (self.heat_setpoint)
 
-        cool_setpt_limits = '{}:{}'.format(thermostat_settings['min_cool_setpoint'],thermostat_settings['max_cool_setpoint'])
-        self.cool_setpoint = Property_Setpoint (node,id='coolsetpoint',name='Cool Setpoint',data_format=cool_setpt_limits,unit=thermostat_settings['units'],value=thermostat_settings['cool_setpoint'],set_value = self.set_cool_setpoint)
-        node.add_property (self.cool_setpoint)
+        if 'cool_setpoint' in thermostat_settings:
+            cool_setpt_limits = '{}:{}'.format(thermostat_settings['min_cool_setpoint'],thermostat_settings['max_cool_setpoint'])
+            self.cool_setpoint = Property_Setpoint (node,id='coolsetpoint',name='Cool Setpoint',data_format=cool_setpt_limits,unit=thermostat_settings['units'],value=thermostat_settings['cool_setpoint'],set_value = self.set_cool_setpoint)
+            node.add_property (self.cool_setpoint)
 
-        self.system_mode = Property_Enum (node,id='systemmode',name='System Mode',data_format=','.join(thermostat_settings['system_modes']),value=thermostat_settings['system_mode'],set_value =self.set_system_mode)
-        node.add_property (self.system_mode)
+        if 'system_mode' in thermostat_settings:
+            self.system_mode = Property_Enum (node,id='systemmode',name='System Mode',data_format=','.join(thermostat_settings['system_modes']),value=thermostat_settings['system_mode'],set_value =self.set_system_mode)
+            node.add_property (self.system_mode)
 
-        self.fan_mode = Property_Enum (node,id='fanmode',name='Fan Mode',data_format=','.join(thermostat_settings['fan_modes']),value=thermostat_settings['fan_mode'],set_value=self.set_fan_mode)
-        node.add_property (self.fan_mode)
+        if 'fan_mode' in thermostat_settings:
+            self.fan_mode = Property_Enum (node,id='fanmode',name='Fan Mode',data_format=','.join(thermostat_settings['fan_modes']),value=thermostat_settings['fan_mode'],set_value=self.set_fan_mode)
+            node.add_property (self.fan_mode)
 
-        self.hold_mode = Property_Enum (node,id='holdmode',name='Hold Mode',data_format=','.join(thermostat_settings['hold_modes']),value=thermostat_settings['hold_mode'],set_value = self.set_hold_mode)
-        node.add_property (self.hold_mode)
+        if 'hold_mode' in thermostat_settings:
+            self.hold_mode = Property_Enum (node,id='holdmode',name='Hold Mode',data_format=','.join(thermostat_settings['hold_modes']),value=thermostat_settings['hold_mode'],set_value = self.set_hold_mode)
+            node.add_property (self.hold_mode)
 
         node = (Node_Base(self,'status','Status','status'))
         self.add_node (node)
